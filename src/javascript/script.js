f630506ce59e97f5235e094d03c0a468c5b8b8da
Celso Netto <celso.bmnetto1@gmail.com>
2024-11-28 20:12:52 -0300

    navbar funcional/ bootstrap adicionado

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -4,3 +4,12 @@ $(document).ready(function() {
         $('#mobile-btn').find('i').toggleClass('fa-x');
     });
 });
+
+document.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) { // Adiciona classe se rolar mais de 50px
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
